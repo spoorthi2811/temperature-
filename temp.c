d2b5df7f2fbbb3f559411418b41d255d94989e5b
spoorthi2811 <spoorthi.sharma@s.amity.edu.com>
2025-07-14 12:38:18 +0530

    fahernite to celcius

---
+++ temp.c
@@ -0,0 +1,15 @@
+#include stdio .h 
+int main()
+{
+	float celsius,farhenheit;
+	print f("enter temperature in celcius:");
+	farhenheit =( celsius*9/5)+32
+
+		printf("Enter temperature in Fahrenheit: ");
+	 scanf("%f", &fahrenheit);
+	 celsius = (fahrenheit - 32) * 5 / 9;
+        printf("%.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
+	printf("Temperature in Celsius: %.2f\n", celsius);
+	return 0;
+}
+
