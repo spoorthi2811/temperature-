27d9642f0727e96ddbd508f437ac80fd035a7730
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-07-28 12:05:23 +0530

    Added While to asigne a work

--- reverse.c
+++ reverse.c
@@ -3,3 +3,11 @@ int main() {
   int n, reverse = 0, remainder, original;
   printf("Enter an integer: ");
   scanf("%d", &n);
+  original = n;
+
+  while (n != 0) {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+  }
+
